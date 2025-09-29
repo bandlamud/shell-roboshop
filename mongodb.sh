b672f290613b942d262a5160c6474101ad8371ec
bharathchandu302 <bharathchandu302@gmail.com>
2025-09-29 20:02:14 +0530

    instaling mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -6,26 +6,24 @@ Y="\e[33m"
 N="\e[0m"
 
 USERID=$(id -u)
-echo "execution of  script timing is: $(date)"
 LOG_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME="$( echo $0 | cut -d "." -f1 )" 
-SCRIPT_DIR=$PWD
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
 mkdir -p $LOG_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: Please run the script with root Privilages"
-    exit 1
+    exit 1 # failure is other than 0
 fi
 
 VALIDATE() { # functions receive inputs through args just like shell script args
-if [ $1 -ne 0 ]; then
-    echo -e  " $2 ... $R FAILURE $N" &>>$LOG_FILE
-    exit 1
-else
-    echo -e " $2 ... $R SUCCESS $N" &>>$LOG_FILE
-fi
+    if [ $1 -ne 0 ]; then
+        echo -e  "$2 ... $R FAILURE $N" &>>$LOG_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" &>>$LOG_FILE
+    fi
 }
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo
@@ -40,7 +38,7 @@ VALIDATE $? "Enable MangoDB"
 systemctl start mongod  &>>$LOG_FILE
 VALIDATE $? "Start MangoDB"
 
-sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mongod.conf
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 
 systemctl restart mongod  &>>$LOG_FILE
 VALIDATE $? "Restarted  MangoDB"
