56a8c1f6884e1b4679e1e2e2270489bbb3654568
bharathchandu302 <bharathchandu302@gmail.com>
2025-09-29 20:31:51 +0530

    ec2 instance creation

--- roboshop.sh
+++ roboshop.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-0f110578dbed67965"
-ZONE_ID="Z00363011Z6DCTPYPRFG8"
+SG_ID="sg-0f110578dbed67965" # replace with your SG ID
+ZONE_ID="Z00363011Z6DCTPYPRFG8" # replace with your ID
 DOMAIN_NAME="daws89s.fun"
 
-for instance in $@
+for instance in $@ # mongodb redis mysql
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
-#Get Private IP
-if [ $instance != "frontend" ]; then
-    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
-    RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws89s.fun
-else
-    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-    RECORD_NAME="$DOMAIN_NAME" # daws89s.fun
-fi
+
+    # Get Private IP
+    if [ $instance != "frontend" ]; then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws86s.fun
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$DOMAIN_NAME" # daws86s.fun
+    fi
+
     echo "$instance: $IP"
+
     aws route53 change-resource-record-sets \
-  --hosted-zone-id $ZONE_ID \
-  --change-batch '
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
     {
         "Comment": "Updating record set"
         ,"Changes": [{
@@ -36,4 +39,3 @@ fi
     }
     '
 done
-
