e5d8893d6e8119bcd387fc561294f516430e3ef5
bharathchandu302 <bharathchandu302@gmail.com>
2025-09-30 20:48:29 +0530

    installing cart

---
+++ cart.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u)
+LOG_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME="$( echo $0 | cut -d "." -f1 )" 
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.daws89s.fun
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+
+mkdir -p $LOG_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run the script with root Privilages"
+    exit 1 # failure is other than 0
+fi
+
+VALIDATE() { # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e  "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
+
+#### Nodejs
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "DIsabling Nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enabling Nodejs"
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing Nodejs"
+
+id roboshop
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    VALIDATE $? "Craeting system user"
+else
+    echo -e "User alredy exist .... $Y SKIPPING $N"
+fi
+
+mkdir -p /app 
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip  &>>$LOG_FILE
+VALIDATE $? "Downloading cart application"
+
+cd /app 
+VALIDATE $? "Changing app directory"
+
+rm -rf /app/*
+VALIDATE $? "Removing exitsing code"
+
+unzip /tmp/cart.zip &>>$LOG_FILE
+VALIDATE $? "Unzip cart"
+ 
+npm install &>>$LOG_FILE
+VALIDATE $? "Installing dependencys"
+
+cp $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service &>>$LOG_FILE
+VALIDATE $? "copying cart.service"
+
+systemctl daemon-reload
+systemctl enable cart &>>$LOG_FILE
+VALIDATE $? "Enable cart"
+
+systemctl restart cart
+VALIDATE $? "Restarted cart"
