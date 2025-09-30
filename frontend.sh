335365dd5165cd03e0336f7201c86d72a41d5828
bharathchandu302 <bharathchandu302@gmail.com>
2025-09-30 22:04:16 +0530

    installing frontend

--- frontend.sh
+++ frontend.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-USERID=$(id -u)
-LOG_FOLDER="/var/log/shell-roboshop"
-SCRIPT_NAME="$( echo $0 | cut -d "." -f1 )" 
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 SCRIPT_DIR=$PWD
 MONGODB_HOST=mongodb.daws89s.fun
-LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
-mkdir -p $LOG_FOLDER
+mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
 if [ $USERID -ne 0 ]; then
-    echo "ERROR:: Please run the script with root Privilages"
+    echo "ERROR:: Please run this script with root privelege"
     exit 1 # failure is other than 0
 fi
 
-VALIDATE() { # functions receive inputs through args just like shell script args
+VALIDATE(){ # functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-        echo -e  "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
     else
         echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
@@ -29,28 +30,23 @@ VALIDATE() { # functions receive inputs through args just like shell script args
 }
 
 dnf module disable nginx -y &>>$LOG_FILE
-VALIDATE $? "Disbaling Nginx"
-
 dnf module enable nginx:1.24 -y &>>$LOG_FILE
-VALIDATE $? "Enabling Nginx"
-
 dnf install nginx -y &>>$LOG_FILE
 VALIDATE $? "Installing Nginx"
 
 systemctl enable nginx  &>>$LOG_FILE
-VALIDATE $? "Enabling nginx"
+systemctl start nginx 
+VALIDATE $? "Starting Nginx"
 
-systemctl start nginx &>>$LOG_FILE
-VALIDATE $? "Started" 
-
-rm -rf /usr/share/nginx/html/*  &>>$LOG_FILE
-VALIDATE $? "Rmoving HTML content"
-
+rm -rf /usr/share/nginx/html/* 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip &>>$LOG_FILE
+VALIDATE $? "Downloading frontend"
 
-cd /usr/share/nginx/html  &>>$LOG_FILE
-unzip /tmp/frontend.zip
+rm -rf /etc/nginx/nginx.conf
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
+VALIDATE $? "Copying nginx.conf"
 
-cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf &>>$LOG_FILE
-
-systemctl restart nginx  &>>$LOG_FILE
+systemctl restart nginx 
+VALIDATE $? "Restarting Nginx"
