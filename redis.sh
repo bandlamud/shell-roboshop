6eb94a67f232352a9e7a7fde3eb7001ad8377631
bharathchandu302 <bharathchandu302@gmail.com>
2025-09-30 19:20:21 +0530

    installing redis

---
+++ redis.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u)
+LOG_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME="$( echo $0 | cut -d "." -f1 )" 
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+START_TIME=$(date +%s)
+
+mkdir -p $LOG_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run the script with root Privilages"
+    exit 1 # failure is other than 0
+fi
+
+VALIDATE() { # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e  "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
+dnf module disable redis -y &>>$LOG_FILE
+VALIDATE $? "Dissbling redis"
+dnf module enable redis:7 -y &>>$LOG_FILE
+VALIDATE $? "Enabling  redis"
+
+dnf install redis -y &>>$LOG_FILE
+VALIDATE $? "Installing redis"
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e 'protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Allowing remote connection to redis"
+
+systemctl enable redis &>>$LOG_FILE
+VALIDATE $? "Enabling  redis"
+systemctl start redis 
+VALIDATE $? "Starting redis" &>>$LOG_FILE
+
+END_TIME=$(date +%s)
+TOTAL_TIME=(( $END_TIME-$START_TIM ))
+
+echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
