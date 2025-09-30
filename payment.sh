238386032191f174e2a25e9b7ad154e85faa4113
bharathchandu302 <bharathchandu302@gmail.com>
2025-09-30 21:40:33 +0530

    installing payment

--- payment.sh
+++ payment.sh
@@ -1,75 +1,64 @@
 #!/bin/bash
 
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-USERID=$(id -u)
-LOG_FOLDER="/var/log/shell-roboshop"
-SCRIPT_NAME="$( echo $0 | cut -d "." -f1 )" 
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 SCRIPT_DIR=$PWD
-MONGODB_HOST=mongodb.daws89s.fun
-LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+MONGODB_HOST=mongodb.daws86s.fun
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 MYSQL_HOST=mysql.daws89s.fun
 
-mkdir -p $LOG_FOLDER
+mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
 if [ $USERID -ne 0 ]; then
-    echo "ERROR:: Please run the script with root Privilages"
+    echo "ERROR:: Please run this script with root privelege"
     exit 1 # failure is other than 0
 fi
 
-VALIDATE() { # functions receive inputs through args just like shell script args
+VALIDATE(){ # functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-        echo -e  "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
     else
         echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 
-dnf install python3 gcc python3-devel -y $LOG_FILE
+dnf install python3 gcc python3-devel -y &>>$LOG_FILE
 
-
-id roboshop $LOG_FILE
+id roboshop &>>$LOG_FILE
 if [ $? -ne 0 ]; then
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
-    VALIDATE $? "Craeting system user"
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "Creating system user"
 else
-    echo -e "User alredy exist .... $Y SKIPPING $N"
+    echo -e "User already exist ... $Y SKIPPING $N"
 fi
 
-mkdir -p /app &>>$LOG_FILE
+mkdir -p /app
 VALIDATE $? "Creating app directory"
 
-curl -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip  &>>$LOG_FILE
+curl -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip &>>$LOG_FILE
 VALIDATE $? "Downloading payment application"
 
 cd /app 
-VALIDATE $? "Changing app directory"
+VALIDATE $? "Changing to app directory"
 
 rm -rf /app/*
-VALIDATE $? "Removing exitsing code" &>>$LOG_FILE
+VALIDATE $? "Removing existing code"
 
 unzip /tmp/payment.zip &>>$LOG_FILE
-VALIDATE $? "Unzip payment"
+VALIDATE $? "unzip payment"
 
 pip3 install -r requirements.txt &>>$LOG_FILE
-VALIDATE $? "Installing dependencies"
 
-cp $SCRIPT_DIR/payment.service /etc/systemd/system/payment.service &>>$LOG_FILE
-
-systemctl daemon-reload &>>$LOG_FILE
-VALIDATE $? "Deamon relod"
-
-systemctl enable payment &>>$LOG_FILE
-VALIDATE $? "elabling payment"
-
-dnf install mysql -y  &>>$LOG_FILE
-VALIDATE $? "installing mysql"
-
-
+cp $SCRIPT_DIR/payment.service /etc/systemd/system/payment.service
+systemctl daemon-reload
+systemctl enable payment  &>>$LOG_FILE
 
 systemctl restart payment
-VALIDATE $? "restart payment"
